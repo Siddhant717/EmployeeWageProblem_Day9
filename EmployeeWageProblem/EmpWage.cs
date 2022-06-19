b1e9c8f6902993daa925be0c34dd1610ab1d8b3a
Siddhant <santoshshrivastava39@gmail.com>
2022-06-19 17:09:21 +0530

    initial commit

---
+++ EmployeeWageProblem/EmpWage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    internal class EmpWage
+    {
+        public const int IsPartTime = 2;
+        public const int IsFullTime = 1;
+        public const int IsAbsent = 0;
+
+
+        public void ComputeEmployeeWage(string CompanyName,int WagePerHr,int MaxNummberOFDays,int MaxHrs)
+        {
+            int Totalwage = 0;
+            int Totalhrs = 0;
+            int NumberOfWorkingDays = 20;
+            int PartTimeHr = 4;
+            int FullTimeHr = 8;
+           
+
+            for (int i = 0; Totalhrs <= MaxHrs && i< MaxNummberOFDays; i++)
+            {
+                Random random = new Random();
+                int number = random.Next(0, 3);
+                switch (number)
+                {
+                    case IsFullTime:
+                        Console.WriteLine("Employee is Full Time");
+                        Totalwage = Totalwage + WagePerHr * FullTimeHr;
+                        Totalhrs += FullTimeHr;
+                       
+                        break;
+                    case IsPartTime:
+                        Console.WriteLine("Employee is working part time");
+                        Totalwage = Totalwage + WagePerHr * PartTimeHr;
+                        Totalwage += WagePerHr * PartTimeHr;
+                        Totalhrs += PartTimeHr;
+
+                        break;
+                    case IsAbsent:
+                        Console.WriteLine("Employee is Absent");
+                        break;
+
+
+                }
+                Console.WriteLine("Employee's Total Wage is" + Totalwage);
+                Console.WriteLine("Total hours the employee worked in the month for " + CompanyName + " is " + Totalhrs);
+            }
+        }
+    }
+}
---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWage comapanies = new EmpWage();
+            comapanies.ComputeEmployeeWage("Reliance", 30, 2, 100);
+            comapanies.ComputeEmployeeWage("DMart", 35, 2, 120);
+        }
+    }
+}
